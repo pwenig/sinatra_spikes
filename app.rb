bfda4e3d3d7dfd6cce0a64eeaab2ab8485184b43
Paul Wenig <pwenig@gmail.com>
2014-03-21 10:29:25 -0600

    created show page that takes instance variables

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 
+require './lib/item'
+require './models/items_repository'
+
+
 get '/' do
   erb :index
 end
 
+get '/items' do
+  erb :items
+end
+
+get '/item/:id' do
+  item = ItemsRepository.new.items.first
+  erb :show, :locals => {:item => item}
+
+end
--- lib/item.rb
+++ lib/item.rb
@@ -2,6 +2,7 @@ class Item
   attr_reader :id, :name
 
   def initialize(id, name)
+    #params = params(id)
     @id   = id
     @name = name
   end
